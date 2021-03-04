05df02e9ddf7352379cc46950ef4f1dd1b723237
dnunez10 <diana_nunez10@yahoo.com>
2021-03-04 11:48:04 -0800

    commit tree 5d752484ae2ea6ee8e212862cd9bcc939854ff12

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(driverNames, string){
+    return driverNames.filter( (driverName) => {
+        return driverName.toLowerCase() === string.toLowerCase()
+    })
+    }
+
+function fuzzyMatch(driverNames, name){
+    return driverNames.filter( (driver) => {
+        return driver[0] == name[0]
+    })
+}
+
+function matchName(driverObjects, string){
+    return driverObjects.filter( (object) => {
+        return object.name === string
+    })
+}
